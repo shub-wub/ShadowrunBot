2ebeabcc653b8ce9100e89838c1a6fb94d49098e
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2023-03-03 01:18:44 -0600

    feat: added schemas decorator for simpler imports

--- src/operations/commands/leaderboard.ts
+++ src/operations/commands/leaderboard.ts
@@ -1,130 +1,184 @@
 import { getThemeColor, mongoError } from "#utilities";
-import { CommandInteraction, CacheType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, TextChannel, Client, BaseInteraction, ButtonInteraction } from "discord.js";
+import {
+	CommandInteraction,
+	CacheType,
+	EmbedBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	TextChannel,
+	Client,
+	BaseInteraction,
+	ButtonInteraction,
+} from "discord.js";
 import { IGuild, IPlayer, ILeaderboard } from "../../types";
-import Guild from '../../schemas/guild';
-import Leaderboard from '../../schemas/leaderboard';
+import Guild from "@schemas/guild";
+import Leaderboard from "@schemas/leaderboard";
 import { MongooseError } from "mongoose";
 
 // Define a function to create a message embed with a given page of players
-export const createLeaderboardEmbed = async (pageNumber: number, players: IPlayer[], playersPerPage: number, device: string, guildId: string): Promise<any> => {
-    const start = (pageNumber - 1) * playersPerPage;
-    const end = start + playersPerPage;
-    const pagePlayers = players.slice(start, end);
-    const guildRecord = await Guild.findOne<IGuild>({ guildId: guildId });
-    if(!guildRecord) return;
-    type Field = {
-        name: string;
-        value: string;
-        inline: boolean;
-      }
-    var fields: Field[] = [];
+export const createLeaderboardEmbed = async (
+	pageNumber: number,
+	players: IPlayer[],
+	playersPerPage: number,
+	device: string,
+	guildId: string
+): Promise<any> => {
+	const start = (pageNumber - 1) * playersPerPage;
+	const end = start + playersPerPage;
+	const pagePlayers = players.slice(start, end);
+	const guildRecord = await Guild.findOne<IGuild>({ guildId: guildId });
+	if (!guildRecord) return;
+	type Field = {
+		name: string;
+		value: string;
+		inline: boolean;
+	};
+	var fields: Field[] = [];
 
-    if (device == "mobile") {
-        for (let i = 0; i < pagePlayers.length; i++) {
-            const playerPlace = i + 1 + (10 * (pageNumber - 1));
-            var wlr = pagePlayers[i].wins / (pagePlayers[i].wins + pagePlayers[i].losses);
-            var emoji = "";
-            if(pagePlayers[i].rating <= guildRecord.bronzeMax) {
-                emoji = `<:bronze:${guildRecord.bronzeEmojiId}>`;
-            } else if (pagePlayers[i].rating <= guildRecord.silverMax) {
-                emoji = `<:silver:${guildRecord.silverEmojiId}>`;
-            } else if (pagePlayers[i].rating <= guildRecord.goldMax) {
-                emoji = `<:gold:${guildRecord.goldEmojiId}>`;
-            } else if (pagePlayers[i].rating <= guildRecord.platinumMax) {
-                emoji = `<:platinum:${guildRecord.platinumEmojiId}>`;
-            } else if (pagePlayers[i].rating <= guildRecord.diamondMax) {
-                emoji = `<:diamond:${guildRecord.diamondEmojiId}>`;
-            }
-            fields.push({name: " ", value: `**${playerPlace}.** ${emoji} <@${pagePlayers[i].discordId}> - ${pagePlayers[i].rating} - ${wlr}`, inline: false});
-        }
-    } else if (device == "pc") {
-        var names = "";
-        var ratings = "";
-        var winlossratio = "";
-        for (let i = 0; i < pagePlayers.length; i++) {
-            const playerPlace = i + 1 + (10 * (pageNumber - 1));
-            var wlr = pagePlayers[i].wins / (pagePlayers[i].wins + pagePlayers[i].losses);
-            var emoji = "";
-            if(pagePlayers[i].rating <= guildRecord.bronzeMax) {
-                emoji = `<:bronze:${guildRecord.bronzeEmojiId}>`;
-            } else if (pagePlayers[i].rating <= guildRecord.silverMax) {
-                emoji = `<:silver:${guildRecord.silverEmojiId}>`;
-            } else if (pagePlayers[i].rating <= guildRecord.goldMax) {
-                emoji = `<:gold:${guildRecord.goldEmojiId}>`;
-            } else if (pagePlayers[i].rating <= guildRecord.platinumMax) {
-                emoji = `<:platinum:${guildRecord.platinumEmojiId}>`;
-            } else if (pagePlayers[i].rating <= guildRecord.diamondMax) {
-                emoji = `<:diamond:${guildRecord.diamondEmojiId}>`;
-            }
-            wlr = Math.round(wlr * 100) / 100
-            names += `**${playerPlace}.** ${emoji} <@${pagePlayers[i].discordId}>\n`;
-            ratings += `${pagePlayers[i].rating}\n`;
-            winlossratio += `${wlr}\n`;
-        }
-        fields.push({name: "Name", value: names, inline: true});
-        fields.push({name: "Rating", value: ratings, inline: true});
-        fields.push({name: "Win Loss Ratio", value: winlossratio, inline: true});
-    }
-    const embed: any = new EmbedBuilder()
-        .setTitle('__**ELO Leaderboard**__')
-        .setColor(getThemeColor("embed"))
-        .setFooter({ text: `Page ${pageNumber} of ${Math.ceil(players.length / playersPerPage)}` })
-        .addFields(fields);
-    return embed;
+	if (device == "mobile") {
+		for (let i = 0; i < pagePlayers.length; i++) {
+			const playerPlace = i + 1 + 10 * (pageNumber - 1);
+			var wlr =
+				pagePlayers[i].wins /
+				(pagePlayers[i].wins + pagePlayers[i].losses);
+			var emoji = "";
+			if (pagePlayers[i].rating <= guildRecord.bronzeMax) {
+				emoji = `<:bronze:${guildRecord.bronzeEmojiId}>`;
+			} else if (pagePlayers[i].rating <= guildRecord.silverMax) {
+				emoji = `<:silver:${guildRecord.silverEmojiId}>`;
+			} else if (pagePlayers[i].rating <= guildRecord.goldMax) {
+				emoji = `<:gold:${guildRecord.goldEmojiId}>`;
+			} else if (pagePlayers[i].rating <= guildRecord.platinumMax) {
+				emoji = `<:platinum:${guildRecord.platinumEmojiId}>`;
+			} else if (pagePlayers[i].rating <= guildRecord.diamondMax) {
+				emoji = `<:diamond:${guildRecord.diamondEmojiId}>`;
+			}
+			fields.push({
+				name: " ",
+				value: `**${playerPlace}.** ${emoji} <@${pagePlayers[i].discordId}> - ${pagePlayers[i].rating} - ${wlr}`,
+				inline: false,
+			});
+		}
+	} else if (device == "pc") {
+		var names = "";
+		var ratings = "";
+		var winlossratio = "";
+		for (let i = 0; i < pagePlayers.length; i++) {
+			const playerPlace = i + 1 + 10 * (pageNumber - 1);
+			var wlr =
+				pagePlayers[i].wins /
+				(pagePlayers[i].wins + pagePlayers[i].losses);
+			var emoji = "";
+			if (pagePlayers[i].rating <= guildRecord.bronzeMax) {
+				emoji = `<:bronze:${guildRecord.bronzeEmojiId}>`;
+			} else if (pagePlayers[i].rating <= guildRecord.silverMax) {
+				emoji = `<:silver:${guildRecord.silverEmojiId}>`;
+			} else if (pagePlayers[i].rating <= guildRecord.goldMax) {
+				emoji = `<:gold:${guildRecord.goldEmojiId}>`;
+			} else if (pagePlayers[i].rating <= guildRecord.platinumMax) {
+				emoji = `<:platinum:${guildRecord.platinumEmojiId}>`;
+			} else if (pagePlayers[i].rating <= guildRecord.diamondMax) {
+				emoji = `<:diamond:${guildRecord.diamondEmojiId}>`;
+			}
+			wlr = Math.round(wlr * 100) / 100;
+			names += `**${playerPlace}.** ${emoji} <@${pagePlayers[i].discordId}>\n`;
+			ratings += `${pagePlayers[i].rating}\n`;
+			winlossratio += `${wlr}\n`;
+		}
+		fields.push({ name: "Name", value: names, inline: true });
+		fields.push({ name: "Rating", value: ratings, inline: true });
+		fields.push({
+			name: "Win Loss Ratio",
+			value: winlossratio,
+			inline: true,
+		});
+	}
+	const embed: any = new EmbedBuilder()
+		.setTitle("__**ELO Leaderboard**__")
+		.setColor(getThemeColor("embed"))
+		.setFooter({
+			text: `Page ${pageNumber} of ${Math.ceil(
+				players.length / playersPerPage
+			)}`,
+		})
+		.addFields(fields);
+	return embed;
 };
 
 // Set up a button row with pagination buttons
-export const createLeaderboardButtonRow = (pageNumber: number, players: IPlayer[], playersPerPage: number): ActionRowBuilder<ButtonBuilder> => {
-    const buttonRow: ActionRowBuilder<ButtonBuilder> = new ActionRowBuilder<ButtonBuilder>();
-    const previousButton = new ButtonBuilder()
-        .setCustomId('previous')
-        .setLabel('Previous')
-        .setStyle(ButtonStyle.Secondary)
-        .setDisabled(pageNumber === 1);
-    const nextButton = new ButtonBuilder()
-        .setCustomId('next')
-        .setLabel('Next')
-        .setStyle(ButtonStyle.Secondary)
-        .setDisabled(pageNumber === Math.ceil(players.length / playersPerPage));
-    buttonRow.addComponents(previousButton, nextButton);
-    return buttonRow;
+export const createLeaderboardButtonRow = (
+	pageNumber: number,
+	players: IPlayer[],
+	playersPerPage: number
+): ActionRowBuilder<ButtonBuilder> => {
+	const buttonRow: ActionRowBuilder<ButtonBuilder> =
+		new ActionRowBuilder<ButtonBuilder>();
+	const previousButton = new ButtonBuilder()
+		.setCustomId("previous")
+		.setLabel("Previous")
+		.setStyle(ButtonStyle.Secondary)
+		.setDisabled(pageNumber === 1);
+	const nextButton = new ButtonBuilder()
+		.setCustomId("next")
+		.setLabel("Next")
+		.setStyle(ButtonStyle.Secondary)
+		.setDisabled(pageNumber === Math.ceil(players.length / playersPerPage));
+	buttonRow.addComponents(previousButton, nextButton);
+	return buttonRow;
 };
 
-export const leaderboard = async (interaction: CommandInteraction<CacheType>, client: Client, players: IPlayer[], playersPerPage: number, device: string): Promise<void> => {
-    // Send the initial message with the first page of players and pagination buttons
-    const initialEmbed = await createLeaderboardEmbed(1, players, playersPerPage, device, interaction.guild?.id as string);
-    const buttonRow = createLeaderboardButtonRow(1, players, playersPerPage);
-    
-    var guildRecord = await Guild.findOne<IGuild>({ guildId: interaction.guildId });
-    if(guildRecord) {
-        var channel = await client.channels.fetch(guildRecord.leaderboardChannelId);
-        var message = await (channel as TextChannel).send({
-            embeds: [initialEmbed], 
-            components: [buttonRow]
-        });
-        // new leaderboard
-        try {
-            await new Leaderboard({
-                messageId: message.id,
-                device: device,
-                page: 1
-            }).save();
-        } catch(error) {
-            mongoError(error as MongooseError);
-            await interaction.reply({
-                content: `There was an error adding the leaderboard to the database.`,
-                ephemeral: true
-            });
-            return;
-        }
-        await interaction.reply({
-            content: `The leaderboard has been created. ${message.url}`,
-            ephemeral: true
-        });
-    } else {
-        await interaction.reply({
-            content: `There was no guild record found. Try using /srinitialize first.`,
-            ephemeral: true
-        });
-    }
-}
+export const leaderboard = async (
+	interaction: CommandInteraction<CacheType>,
+	client: Client,
+	players: IPlayer[],
+	playersPerPage: number,
+	device: string
+): Promise<void> => {
+	// Send the initial message with the first page of players and pagination buttons
+	const initialEmbed = await createLeaderboardEmbed(
+		1,
+		players,
+		playersPerPage,
+		device,
+		interaction.guild?.id as string
+	);
+	const buttonRow = createLeaderboardButtonRow(1, players, playersPerPage);
+
+	var guildRecord = await Guild.findOne<IGuild>({
+		guildId: interaction.guildId,
+	});
+	if (guildRecord) {
+		var channel = await client.channels.fetch(
+			guildRecord.leaderboardChannelId
+		);
+		var message = await (channel as TextChannel).send({
+			embeds: [initialEmbed],
+			components: [buttonRow],
+		});
+		// new leaderboard
+		try {
+			await new Leaderboard({
+				messageId: message.id,
+				device: device,
+				page: 1,
+			}).save();
+		} catch (error) {
+			mongoError(error as MongooseError);
+			await interaction.reply({
+				content: `There was an error adding the leaderboard to the database.`,
+				ephemeral: true,
+			});
+			return;
+		}
+		await interaction.reply({
+			content: `The leaderboard has been created. ${message.url}`,
+			ephemeral: true,
+		});
+	} else {
+		await interaction.reply({
+			content: `There was no guild record found. Try using /srinitialize first.`,
+			ephemeral: true,
+		});
+	}
+};
--- src/operations/commands/srinitialize.ts
+++ src/operations/commands/srinitialize.ts
@@ -1,106 +1,157 @@
-import { mongoError} from "#utilities";
-import { CacheType, ChannelType, CommandInteraction, GuildEmoji, MappedGuildChannelTypes, Role, TextChannel } from "discord.js";
-import Guild from "../../schemas/guild";
+import { mongoError } from "#utilities";
+import {
+	CacheType,
+	ChannelType,
+	CommandInteraction,
+	GuildEmoji,
+	MappedGuildChannelTypes,
+	Role,
+	TextChannel,
+} from "discord.js";
+import Guild from "@schemas/guild";
 import { MongooseError } from "mongoose";
 import { IGuild } from "../../types";
 
-export const srinitialize = async (interaction: CommandInteraction<CacheType>): Promise<void> => {
-    const existingGuildRecord = await Guild.findOne<IGuild>({ guildId: interaction.guildId });
-    if(existingGuildRecord) {
-        await interaction.reply({
-            content: `srinitialize has already been ran on this server.`,
-            ephemeral: true
-        });
-        return;
-    }
+export const srinitialize = async (
+	interaction: CommandInteraction<CacheType>
+): Promise<void> => {
+	const existingGuildRecord = await Guild.findOne<IGuild>({
+		guildId: interaction.guildId,
+	});
+	if (existingGuildRecord) {
+		await interaction.reply({
+			content: `srinitialize has already been ran on this server.`,
+			ephemeral: true,
+		});
+		return;
+	}
 
-    const bronzeRoleCreate = interaction.guild?.roles.create({ name: 'Bronze', color: "DarkOrange", reason: 'ShadowrunBot Ranked'});
-    const silverRoleCreate = interaction.guild?.roles.create({ name: 'Silver', color: "Greyple", reason: 'ShadowrunBot Ranked'});
-    const goldRoleCreate = interaction.guild?.roles.create({ name: 'Gold', color: "Gold", reason: 'ShadowrunBot Ranked'});
-    const platinumRoleCreate = interaction.guild?.roles.create({ name: 'Platinum', color: "DarkGreen", reason: 'ShadowrunBot Ranked'});
-    const diamondRoleCreate = interaction.guild?.roles.create({ name: 'Diamond', color: "Aqua", reason: 'ShadowrunBot Ranked'});
-    
-    const bronzeEmojiCreate = interaction.guild?.emojis.create({attachment: './assets/bronze.webp', name: 'bronze', reason: 'ShadowrunBot Ranked'});
-    const silverEmojiCreate = interaction.guild?.emojis.create({attachment: './assets/silver.webp', name: 'silver', reason: 'ShadowrunBot Ranked'});
-    const goldEmojiCreate = interaction.guild?.emojis.create({attachment: './assets/gold.webp', name: 'gold', reason: 'ShadowrunBot Ranked'});
-    const platinumEmojiCreate = interaction.guild?.emojis.create({attachment: './assets/platinum.webp', name: 'platinum', reason: 'ShadowrunBot Ranked'});
-    const diamondEmojiCreate = interaction.guild?.emojis.create({attachment: './assets/diamond.webp', name: 'diamond', reason: 'ShadowrunBot Ranked'});
+	const bronzeRoleCreate = interaction.guild?.roles.create({
+		name: "Bronze",
+		color: "DarkOrange",
+		reason: "ShadowrunBot Ranked",
+	});
+	const silverRoleCreate = interaction.guild?.roles.create({
+		name: "Silver",
+		color: "Greyple",
+		reason: "ShadowrunBot Ranked",
+	});
+	const goldRoleCreate = interaction.guild?.roles.create({
+		name: "Gold",
+		color: "Gold",
+		reason: "ShadowrunBot Ranked",
+	});
+	const platinumRoleCreate = interaction.guild?.roles.create({
+		name: "Platinum",
+		color: "DarkGreen",
+		reason: "ShadowrunBot Ranked",
+	});
+	const diamondRoleCreate = interaction.guild?.roles.create({
+		name: "Diamond",
+		color: "Aqua",
+		reason: "ShadowrunBot Ranked",
+	});
 
-    const cat = await interaction.guild?.channels.create({
-        name: "Ranked",
-        type: ChannelType.GuildCategory
-    }) as MappedGuildChannelTypes[ChannelType.GuildCategory];
+	const bronzeEmojiCreate = interaction.guild?.emojis.create({
+		attachment: "./assets/bronze.webp",
+		name: "bronze",
+		reason: "ShadowrunBot Ranked",
+	});
+	const silverEmojiCreate = interaction.guild?.emojis.create({
+		attachment: "./assets/silver.webp",
+		name: "silver",
+		reason: "ShadowrunBot Ranked",
+	});
+	const goldEmojiCreate = interaction.guild?.emojis.create({
+		attachment: "./assets/gold.webp",
+		name: "gold",
+		reason: "ShadowrunBot Ranked",
+	});
+	const platinumEmojiCreate = interaction.guild?.emojis.create({
+		attachment: "./assets/platinum.webp",
+		name: "platinum",
+		reason: "ShadowrunBot Ranked",
+	});
+	const diamondEmojiCreate = interaction.guild?.emojis.create({
+		attachment: "./assets/diamond.webp",
+		name: "diamond",
+		reason: "ShadowrunBot Ranked",
+	});
 
-    const queueChannelCreate = interaction.guild?.channels.create({
-            name: "queue",
-            type: ChannelType.GuildText,
-            parent: cat.id 
-        });
-    const matchChannelCreate = interaction.guild?.channels.create({
-            name: "matches",
-            type: ChannelType.GuildText,
-            parent: cat.id
-        });
-    const leaderboardChannelCreate = interaction.guild?.channels.create({
-            name: "leaderboard",
-            type: ChannelType.GuildText,
-            parent: cat.id
-        });
+	const cat = (await interaction.guild?.channels.create({
+		name: "Ranked",
+		type: ChannelType.GuildCategory,
+	})) as MappedGuildChannelTypes[ChannelType.GuildCategory];
 
-    Promise.all(
-        [
-            queueChannelCreate as Promise<TextChannel>, 
-            matchChannelCreate as Promise<TextChannel>, 
-            leaderboardChannelCreate as Promise<TextChannel>,
-            bronzeEmojiCreate as Promise<GuildEmoji>,
-            silverEmojiCreate as Promise<GuildEmoji>,
-            goldEmojiCreate as Promise<GuildEmoji>,
-            platinumEmojiCreate as Promise<GuildEmoji>,
-            diamondEmojiCreate as Promise<GuildEmoji>,
-            bronzeRoleCreate as Promise<Role>,
-            silverRoleCreate as Promise<Role>,
-            goldRoleCreate as Promise<Role>,
-            platinumRoleCreate as Promise<Role>,
-            diamondRoleCreate as Promise<Role>
-        ]).then(async (results) => {
-            try {
-                await new Guild({
-                    guildId: interaction.commandGuildId as string,
-                    rankedCategoryId: cat.id,
-                    queueChannelId: results[0].id,
-                    matchChannelId: results[1].id,
-                    leaderboardChannelId: results[2].id,
-                    bronzeEmojiId: results[3].id,
-                    silverEmojiId: results[4].id,
-                    goldEmojiId: results[5].id,
-                    platinumEmojiId: results[6].id,
-                    diamondEmojiId: results[7].id,
-                    bronzeRoleId: results[8].id,
-                    silverRoleId: results[9].id,
-                    goldRoleId: results[10].id,
-                    platinumRoleId: results[11].id,
-                    diamondRoleId: results[12].id,
-                    bronzeMin: 0,
-                    bronzeMax: 799,
-                    silverMin: 800,
-                    silverMax: 1099,
-                    goldMin: 1100,
-                    goldMax: 1299,
-                    platinumMin: 1300,
-                    platinumMax: 1499,
-                    diamondMin: 1500,
-                    diamondMax: 3000
-                }).save();
-                await interaction.reply({
-                    content: `Ranked Category was created. Ranked emojis were created. Ranked roles were created.`,
-                    ephemeral: true
-                });
-            } catch(error) {
-                mongoError(error as MongooseError);
-                await interaction.reply({
-                    content: `There was an error creating the guild record in the database.`,
-                    ephemeral: true
-                });
-            }
-    });
-}
+	const queueChannelCreate = interaction.guild?.channels.create({
+		name: "queue",
+		type: ChannelType.GuildText,
+		parent: cat.id,
+	});
+	const matchChannelCreate = interaction.guild?.channels.create({
+		name: "matches",
+		type: ChannelType.GuildText,
+		parent: cat.id,
+	});
+	const leaderboardChannelCreate = interaction.guild?.channels.create({
+		name: "leaderboard",
+		type: ChannelType.GuildText,
+		parent: cat.id,
+	});
+
+	Promise.all([
+		queueChannelCreate as Promise<TextChannel>,
+		matchChannelCreate as Promise<TextChannel>,
+		leaderboardChannelCreate as Promise<TextChannel>,
+		bronzeEmojiCreate as Promise<GuildEmoji>,
+		silverEmojiCreate as Promise<GuildEmoji>,
+		goldEmojiCreate as Promise<GuildEmoji>,
+		platinumEmojiCreate as Promise<GuildEmoji>,
+		diamondEmojiCreate as Promise<GuildEmoji>,
+		bronzeRoleCreate as Promise<Role>,
+		silverRoleCreate as Promise<Role>,
+		goldRoleCreate as Promise<Role>,
+		platinumRoleCreate as Promise<Role>,
+		diamondRoleCreate as Promise<Role>,
+	]).then(async (results) => {
+		try {
+			await new Guild({
+				guildId: interaction.commandGuildId as string,
+				rankedCategoryId: cat.id,
+				queueChannelId: results[0].id,
+				matchChannelId: results[1].id,
+				leaderboardChannelId: results[2].id,
+				bronzeEmojiId: results[3].id,
+				silverEmojiId: results[4].id,
+				goldEmojiId: results[5].id,
+				platinumEmojiId: results[6].id,
+				diamondEmojiId: results[7].id,
+				bronzeRoleId: results[8].id,
+				silverRoleId: results[9].id,
+				goldRoleId: results[10].id,
+				platinumRoleId: results[11].id,
+				diamondRoleId: results[12].id,
+				bronzeMin: 0,
+				bronzeMax: 799,
+				silverMin: 800,
+				silverMax: 1099,
+				goldMin: 1100,
+				goldMax: 1299,
+				platinumMin: 1300,
+				platinumMax: 1499,
+				diamondMin: 1500,
+				diamondMax: 3000,
+			}).save();
+			await interaction.reply({
+				content: `Ranked Category was created. Ranked emojis were created. Ranked roles were created.`,
+				ephemeral: true,
+			});
+		} catch (error) {
+			mongoError(error as MongooseError);
+			await interaction.reply({
+				content: `There was an error creating the guild record in the database.`,
+				ephemeral: true,
+			});
+		}
+	});
+};
